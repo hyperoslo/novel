6fa8468bb5a72cc8cd630657ef2691e5adff908b
Vadym Markov <markov.vadym@gmail.com>
2016-11-30 23:36:49 +0100

    Add postgres provider as a dependency

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   ],
   dependencies: [
     .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
-    .Package(url: "https://github.com/vapor/postgresql", majorVersion: 1, minor: 1)
+    .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1)
   ],
   exclude: [
     "Config",
