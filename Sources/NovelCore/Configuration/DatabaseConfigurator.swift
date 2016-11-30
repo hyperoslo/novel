3448d483e1247b63b0374df2b3d06ff8fd237175
Vadym Markov <markov.vadym@gmail.com>
2016-11-30 23:55:06 +0100

    Use postgres provider

--- Sources/NovelCore/Configuration/DatabaseConfigurator.swift
+++ Sources/NovelCore/Configuration/DatabaseConfigurator.swift
@@ -4,7 +4,8 @@ import VaporPostgreSQL
 
 struct DatabaseConfigurator: Configurator {
 
-  func configure(drop: Droplet) {
+  func configure(drop: Droplet) throws {
+    try drop.addProvider(VaporPostgreSQL.Provider.self)
     drop.preparations = [User.self, Entry.self, Chapter.self, Field.self, Content.self]
   }
 }
