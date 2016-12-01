e26a47a15a1d2a9dbf2b435b7e925e51cb93cb9e
Vadym Markov <markov.vadym@gmail.com>
2016-12-02 00:24:11 +0100

    Implement entry controller functionality

--- Sources/NovelAdmin/Controllers/EntryController.swift
+++ Sources/NovelAdmin/Controllers/EntryController.swift
@@ -5,9 +5,63 @@ import NovelCore
 final class EntryController: Controller {
 
   func index(request: Request) throws -> ResponseRepresentable {
+    let context = [
+      "chapters": try Chapter.all().makeNode(),
+      "entries": try Entry.all().makeNode()
+    ]
+
     return try drop.view.make(
       Template.Main.entry.index,
-      makeContext(request: request)
+      makeContext(from: context, request: request)
     )
+  }
+
+  func index(request: Request, chapter: Chapter) throws -> ResponseRepresentable {
+    let context = [
+      "chapters": try Chapter.all().makeNode(),
+      "entries": try Entry.all().makeNode()
+    ]
+
+    return try drop.view.make(
+      Template.Main.entry.index,
+      makeContext(from: context, request: request)
+    )
+  }
+
+  func new(request: Request, chapter: Chapter) throws -> ResponseRepresentable {
+    let context = [
+      "chapter": try chapter.makeNode()
+    ]
+
+    return try drop.view.make(
+      Template.Main.entry.new,
+      makeContext(from: context, request: request)
+    )
+  }
+
+  func store(request: Request, chapter: Chapter) throws -> ResponseRepresentable {
+    guard let node = request.formURLEncoded else {
+      throw Abort.badRequest
+    }
+
+    do {
+      try EntryManager().create(node: node)
+    } catch let error as InputError {
+      let context = [
+        "data": node,
+        "chapter": try chapter.makeNode(),
+        "flash": "Please fill the required fields",
+        "errors": Node.object(error.errors)
+      ]
+
+      return try drop.view.make(
+        Template.Main.entry.new,
+        makeContext(from: context, request: request)
+      )
+    } catch {
+      throw Abort.serverError
+    }
+
+    return redirect(.entries)
   }
 }
