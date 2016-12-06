50c02641f9c180f94588f3153923110799593632
Vadym Markov <markov.vadym@gmail.com>
2016-12-07 00:23:49 +0100

    Implement session model

---
+++ Sources/NovelCore/Library/SessionCache.swift
@@ -0,0 +1,9 @@
+//
+//  SessionStorage.swift
+//  Novel
+//
+//  Created by Vadym Markov on 06/12/2016.
+//
+//
+
+import Foundation
---
+++ Sources/NovelCore/Models/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Novel
+//
+//  Created by Vadym Markov on 06/12/2016.
+//
+//
+
+import Foundation
