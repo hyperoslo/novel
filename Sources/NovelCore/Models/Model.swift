19bfadd9ff973a097f79882cd04f0f4e7420e967
Vadym Markov <markov.vadym@gmail.com>
2016-12-07 00:24:06 +0100

    Move model protocol to a separate file

--- Sources/NovelCore/Models/Model.swift
+++ Sources/NovelCore/Models/Model.swift
@@ -1,9 +1,19 @@
-//
-//  Model.swift
-//  Novel
-//
-//  Created by Vadym Markov on 06/12/2016.
-//
-//
+import Vapor
+import Fluent
 
-import Foundation
+public protocol Model: Vapor.Model {
+  func validate() throws
+}
+
+extension Model {
+
+  public func updated(from node: Node, exists: Bool = false) throws -> Self {
+    var updatedNode = try makeNode()
+    updatedNode.merge(with: node)
+
+    var model = try Self(node: updatedNode)
+    model.exists = exists
+
+    return model
+  }
+}
