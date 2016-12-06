b62b4da0d1cf846b57351fb4777a22399d5aa5e4
Vadym Markov <markov.vadym@gmail.com>
2016-12-07 00:24:42 +0100

    Implement session cache

--- Sources/NovelCore/Library/SessionCache.swift
+++ Sources/NovelCore/Library/SessionCache.swift
@@ -1,9 +1,41 @@
-//
-//  SessionStorage.swift
-//  Novel
-//
-//  Created by Vadym Markov on 06/12/2016.
-//
-//
-
 import Foundation
+import Cache
+import Node
+
+public final class SessionCache: CacheProtocol {
+  private var storage: [String: Node]
+
+  public init() {
+    storage = [:]
+  }
+
+  public func get(_ key: String) throws -> Node? {
+    if let token = storage[key] {
+      return token
+    }
+
+    let session = try Session.query().filter(Session.Key.token.value, key).first()
+    return try session?.user().get()?.id
+  }
+
+  public func set(_ key: String, _ value: Node) throws {
+    storage[key] = value
+
+    let date = Date()
+    let node: Node = [
+      Session.Key.token.value: Node.string(key),
+      Session.Key.createdAt.value: try Int(date.timeIntervalSince1970).makeNode(),
+      Session.Key.updatedAt.value: try Int(date.timeIntervalSince1970).makeNode(),
+      Session.Key.userId.value: value
+    ]
+
+    try Session.query().filter(Session.Key.userId.value, value).delete()
+    var session = try Session(node: node)
+    try session.save()
+  }
+
+  public func delete(_ key: String) throws {
+    storage.removeValue(forKey: key)
+    try Session.query().filter(Session.Key.token.value, key).delete()
+  }
+}
