daef998bc5474f9fffa3c4e318d0f52a6f3a1567
Vadym Markov <markov.vadym@gmail.com>
2016-12-08 23:29:54 +0100

    Setup prototype routing

--- Sources/NovelAPI/Configuration/RouteConfigurator.swift
+++ Sources/NovelAPI/Configuration/RouteConfigurator.swift
@@ -10,11 +10,19 @@ struct RouteConfigurator: Configurator {
     drop.group(Route.root) { root in
       // Entries
       root.group(Route.entries.relative) { entries in
-        let entryController = EntryController(drop: drop)
+        let controller = EntryController(drop: drop)
 
-        entries.get(handler: entryController.index)
-        entries.get(String.self, handler: entryController.index)
-        entries.get(String.self, Int.self, handler: entryController.show)
+        entries.get(handler: controller.index)
+        entries.get(String.self, handler: controller.index)
+        entries.get(String.self, Int.self, handler: controller.show)
+      }
+
+      // Prototypes
+      root.group(Route.prototypes.relative) { entries in
+        let controller = PrototypeController(drop: drop)
+
+        entries.get(handler: controller.index)
+        entries.get(String.self, handler: controller.show)
       }
     }
   }
