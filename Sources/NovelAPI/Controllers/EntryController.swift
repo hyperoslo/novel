be6632d136a51af364f9c0a5dabb424e6583f38c
Vadym Markov <markov.vadym@gmail.com>
2016-12-08 22:19:30 +0100

    Implement API for entries

--- Sources/NovelAPI/Controllers/EntryController.swift
+++ Sources/NovelAPI/Controllers/EntryController.swift
@@ -1,9 +1,31 @@
-//
-//  EntryController.swift
-//  Novel
-//
-//  Created by Vadym Markov on 08/12/2016.
-//
-//
+import Vapor
+import HTTP
+import NovelCore
 
-import Foundation
+final class EntryController: Controller {
+
+  func index(request: Request) throws -> ResponseRepresentable {
+    let context: Context = [
+      "entries": try EntryPresenter.makeNodes(from: try Entry.all())
+    ]
+
+    return try JSON(node: context)
+  }
+
+  func index(request: Request, prototype: Prototype) throws -> ResponseRepresentable {
+    let context = [
+      "prototypes": try Prototype.all().makeNode(),
+      "entries": try EntryPresenter.makeNodes(from: try prototype.entries().all())
+    ]
+
+    return try JSON(node: context)
+  }
+
+  func show(request: Request, prototype: Prototype, entry: Entry) throws -> ResponseRepresentable {
+    let context: Context = [
+      "entry": try EntryPresenter(model: entry).makeNode()
+    ]
+
+    return try JSON(node: context)
+  }
+}
