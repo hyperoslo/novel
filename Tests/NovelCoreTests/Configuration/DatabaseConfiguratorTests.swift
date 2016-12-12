98429e6f4a62eed9e5ee452df8742991df343d5e
Vadym Markov <markov.vadym@gmail.com>
2016-12-13 00:21:44 +0100

    Configuration tests

---
+++ Tests/NovelCoreTests/Configuration/DatabaseConfiguratorTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+import Vapor
+import VaporPostgreSQL
+@testable import NovelCore
+
+class DatabaseConfiguratorTests: XCTestCase {
+
+  static let allTests = [
+    ("testConfigure", testConfigure),
+  ]
+
+  var configurator: DatabaseConfigurator!
+
+  override func setUp() {
+    super.setUp()
+    configurator = DatabaseConfigurator()
+  }
+
+  func testConfigure() throws {
+    let drop = Droplet()
+    try configurator.configure(drop: drop)
+    XCTAssertTrue(drop.providers.contains(where: { $0 is  VaporPostgreSQL.Provider }))
+    XCTAssertEqual(drop.preparations.count, 7)
+  }
+}
---
+++ Tests/NovelCoreTests/Configuration/MiddlewareConfiguratorTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+import Vapor
+import Auth
+@testable import NovelCore
+
+class MiddlewareConfiguratorTests: XCTestCase {
+
+  static let allTests = [
+    ("testConfigure", testConfigure),
+  ]
+
+  var configurator: MiddlewareConfigurator!
+
+  override func setUp() {
+    super.setUp()
+    configurator = MiddlewareConfigurator()
+  }
+
+  func testConfigure() throws {
+    let drop = Droplet()
+    try configurator.configure(drop: drop)
+    XCTAssertTrue(drop.middleware.contains(where: { $0 is AuthMiddleware<NovelCore.User> }))
+  }
+}
