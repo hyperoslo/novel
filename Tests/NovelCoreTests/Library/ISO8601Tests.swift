b8706610c09cdd6c68bd2e036d80e31e6e2643dc
Vadym Markov <markov.vadym@gmail.com>
2016-12-13 09:36:36 +0100

    Test ISO8601

--- Tests/NovelCoreTests/Library/ISO8601Tests.swift
+++ Tests/NovelCoreTests/Library/ISO8601Tests.swift
@@ -1,9 +1,35 @@
-//
-//  ISO8601Tests.swift
-//  Novel
-//
-//  Created by Vadym Markov on 13/12/2016.
-//
-//
-
+import XCTest
 import Foundation
+@testable import NovelCore
+
+class ISO8601Tests: XCTestCase {
+
+  static let allTests = [
+    ("testInit", testInit),
+    ]
+
+  var iso8601: ISO8601!
+
+  override func setUp() {
+    super.setUp()
+    iso8601 = ISO8601()
+  }
+
+  func testInit() throws {
+    XCTAssertEqual(iso8601.formatter.dateFormat, "yyyy-MM-dd HH:mm:ss")
+  }
+
+  func testDate() throws {
+    let date = Date()
+    let string = iso8601.formatter.string(from: date)
+
+    XCTAssertEqual(date.iso8601, string)
+  }
+
+  func testString() throws {
+    let date = Date()
+    let string = iso8601.formatter.string(from: date)
+
+    XCTAssertEqual(string.iso8601?.iso8601, date.iso8601)
+  }
+}
