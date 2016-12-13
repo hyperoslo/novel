0fa6b3b78d69cf991ac8ad0c9c2cfb18eb53bfec
Vadym Markov <markov.vadym@gmail.com>
2016-12-13 10:38:58 +0100

    Session cache tests

--- Tests/NovelCoreTests/Library/SessionCacheTests.swift
+++ Tests/NovelCoreTests/Library/SessionCacheTests.swift
@@ -1,9 +1,47 @@
-//
-//  SessionCacheTests.swift
-//  Novel
-//
-//  Created by Vadym Markov on 13/12/2016.
-//
-//
+import XCTest
+import Vapor
+import Fluent
+@testable import NovelCore
 
-import Foundation
+class SessionCacheTests: XCTestCase {
+
+  static let allTests = [
+    ("testGet", testGet),
+    ("testSet", testSet),
+    ("testDelete", testDelete)
+  ]
+
+  var cache: SessionCache!
+  let key = "token"
+
+  override func setUp() {
+    super.setUp()
+    Session.database = Database(MemoryDriver())
+    cache = SessionCache()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    Session.database = nil
+  }
+
+  // MARK: - Tests
+
+  func testGet() throws {
+    XCTAssertNil(try cache.get(key))
+    try cache.set(key, 1)
+    XCTAssertEqual(try cache.get(key), 1)
+  }
+
+  func testSet() throws {
+    try cache.set(key, 1)
+    XCTAssertEqual(try cache.get(key), 1)
+  }
+
+  func testDelete() throws {
+    try cache.set(key, 1)
+    XCTAssertEqual(try cache.get(key), 1)
+    try cache.delete(key)
+    XCTAssertNil(try cache.get(key))
+  }
+}
