a240d22c964e8f2566ae32dc548ff8c841ad5be0
Vadym Markov <markov.vadym@gmail.com>
2016-12-21 16:19:22 +0100

    Add tests for text type

--- Tests/NovelCoreTests/Extensions/SchemaTests.swift
+++ Tests/NovelCoreTests/Extensions/SchemaTests.swift
@@ -6,18 +6,20 @@ class SchemaTests: XCTestCase {
 
   static let allTests = [
     ("testTimestamp", testTimestamp),
+    ("testText", testText)
   ]
 
   fileprivate var creator: Schema.Creator!
 
   override func setUp() {
     super.setUp()
-    creator = Schema.Creator("user")
   }
 
   // MARK: - Tests
 
   func testTimestamp() throws {
+    creator = Schema.Creator("user")
+
     creator.timestamp("createdAt", optional: false, unique: false, default: "2016-12-01 23:01:00")
     XCTAssertEqual(creator.fields.count, 1)
 
@@ -34,5 +36,26 @@ class SchemaTests: XCTestCase {
     XCTAssertFalse(field.optional)
     XCTAssertFalse(field.unique)
     XCTAssertEqual(field.default, "2016-12-01 23:01:00")
+  }
+
+  func testText() throws {
+    creator = Schema.Creator("user")
+
+    creator.text("body", optional: false, unique: false, default: "Text")
+    XCTAssertEqual(creator.fields.count, 1)
+
+    let field = creator.fields.first!
+    XCTAssertEqual(field.name, "body")
+
+    switch field.type {
+    case .custom(let string):
+      XCTAssertEqual(string, "text")
+    default:
+      XCTFail("Field type must be `text`")
+    }
+
+    XCTAssertFalse(field.optional)
+    XCTAssertFalse(field.unique)
+    XCTAssertEqual(field.default, "Text")
   }
 }
