a20e8798d78db10056dcc9cff651b2f84a1f032c
Vadym Markov <markov.vadym@gmail.com>
2016-12-21 16:15:58 +0100

    Add a new text type

--- Sources/NovelCore/Extensions/Schema.swift
+++ Sources/NovelCore/Extensions/Schema.swift
@@ -6,4 +6,9 @@ extension Schema.Creator {
                         default: NodeRepresentable? = nil) {
     custom(name, type: "timestamp", optional: optional, unique: unique, default: `default`)
   }
+
+  public func text(_ name: String, optional: Bool = false, unique: Bool = false,
+                   default: NodeRepresentable? = nil) {
+    custom(name, type: "text", optional: optional, unique: unique, default: `default`)
+  }
 }
