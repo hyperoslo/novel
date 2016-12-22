4e46d1192ec55d60cdc191c923d48e8b430bc0ff
Vadym Markov <markov.vadym@gmail.com>
2016-12-22 21:26:35 +0100

    Build json with fields

--- Resources/Web/src/admin/js/chapter.js
+++ Resources/Web/src/admin/js/chapter.js
@@ -1,6 +1,7 @@
 $(function() {
   checkRemoveButton();
 
+  // Add a new field
   $('body').on('click', '#new-field-btn', function(event) {
 		event.preventDefault();
 
@@ -19,6 +20,7 @@ $(function() {
     checkRemoveButton();
 	});
 
+  // Remove a field
   $('body').on('click', 'button.remove-field-btn', function(event) {
     event.preventDefault();
 
@@ -28,8 +30,32 @@ $(function() {
       return false;
     });
   });
+
+  // Build fields json before submit
+  $("#form").submit(function(event) {
+    var fields = [];
+
+    $(this).find('.field-group').each(function() {
+      var field = {
+        "name": $(this).find("input[name='field_names[]']").val(),
+        "handle": $(this).find("input[name='field_handles[]']").val(),
+        "kind": $(this).find("select option:selected").val()
+      };
+
+      var id = $(this).find("input[name='field_ids[]']");
+
+      if (!id.length) {
+        field["id"] = id.val()
+      }
+
+      fields.push(field)
+    });
+
+    $('#fields').val(JSON.stringify(fields));
+  });
 });
 
+// Hide/show remove button
 function checkRemoveButton() {
   var buttons = $("form .remove-field-btn")
 
